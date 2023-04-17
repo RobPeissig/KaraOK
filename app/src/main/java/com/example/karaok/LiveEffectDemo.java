020cb227dc6576bc55d88315219edd57f212118e
JerryLinLinLin <hlin323@wisc.edu>
2023-04-16 21:20:16 -0500

    Add call demo in LiveEffectDemo.java

--- app/src/main/java/com/example/karaok/LiveEffectDemo.java
+++ app/src/main/java/com/example/karaok/LiveEffectDemo.java
@@ -73,6 +73,7 @@ public class LiveEffectDemo extends Activity
     private AudioRecorder audioRecorder;
     private boolean isRecording = false;
     private MediaPlayer player;
+    private VolumeMixer volumeMixer;
     private boolean curPlaying;
     private Button recordButton;
     String songName;
@@ -83,6 +84,8 @@ public class LiveEffectDemo extends Activity
         Bundle bundle = getIntent().getExtras();
         songName= bundle.getString("songName");
         audioRecorder = new AudioRecorder();
+        player = new MediaPlayer();
+        volumeMixer = new VolumeMixer(player);
         ld1 = findViewById(R.id.ld1);
         ld2 = findViewById(R.id.ld2);
         toggleEffectButton = findViewById(R.id.button_toggle_effect);
@@ -190,6 +193,8 @@ public class LiveEffectDemo extends Activity
             return;
         }
 //        LiveEffectEngine.setGain(3.0f);
+        volumeMixer.setLiveEffectVolume(2.0f);
+
         boolean success = LiveEffectEngine.setEffectOn(true);
         if (success) {
             //OLD: mp.start();
@@ -341,8 +346,8 @@ public class LiveEffectDemo extends Activity
 
             @Override
             public void onSuccess(Uri downloadUrl){
-                    player = new MediaPlayer();
                 try {
+                    volumeMixer.setMusicVolume(0.5f);
                     player.setDataSource(downloadUrl.toString());
                     player.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
                         @Override

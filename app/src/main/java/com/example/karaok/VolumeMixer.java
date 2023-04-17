dbca927020dd9739319c74d57961ca2e87eba491
JerryLinLinLin <hlin323@wisc.edu>
2023-04-16 21:13:28 -0500

    Add VolumeMixer API

--- app/src/main/java/com/example/karaok/VolumeMixer.java
+++ app/src/main/java/com/example/karaok/VolumeMixer.java
@@ -1,4 +1,33 @@
 package com.example.karaok;
 
+import android.media.MediaPlayer;
+
 public class VolumeMixer {
+
+    private MediaPlayer mediaPlayer;
+
+    private float musicVolume = 1.0f;
+    private float liveEffectVolume = 1.0f;
+
+    public VolumeMixer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public void setMusicVolume(float volume) {
+        musicVolume = volume;
+        mediaPlayer.setVolume(musicVolume, musicVolume);
+    }
+
+    public void setLiveEffectVolume(float volume) {
+        liveEffectVolume = volume;
+        LiveEffectEngine.setGain(liveEffectVolume);
+    }
+
+    public float getLiveEffectVolume() {
+        return liveEffectVolume;
+    }
+
+    public float getMusicVolume() {
+        return musicVolume;
+    }
 }

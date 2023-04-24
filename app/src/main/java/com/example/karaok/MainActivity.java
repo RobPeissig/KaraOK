d68def22d0c8b3b8534d3d278b431656cbac91e3
PatrickTheMerchant <71906962+PatrickTheMerchant@users.noreply.github.com>
2023-04-24 17:37:22 -0500

    Merged FirebaseAuth with main (CAUTION! HAVE NOT TESTED FUNCTIONALITY)

--- app/src/main/java/com/example/karaok/MainActivity.java
+++ app/src/main/java/com/example/karaok/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
